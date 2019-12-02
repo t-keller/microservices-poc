09b2b85054520f092f7262e1f8ffbfb22e27d07d
Thomas <thomas@t-keller.fr>
2019-12-02 21:05:54 +0100

    Implement Lombok for user-service

--- user-service/src/main/java/com/example/userservice/entities/User.java
+++ user-service/src/main/java/com/example/userservice/entities/User.java
@@ -5,7 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 @Entity(name = "users")
+@Data
 public class User {
 
 	@Id
@@ -13,32 +16,4 @@ public class User {
 	private int id;
 	private String firstname;
 	private String lastname;
-
-	public User() {
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getFirstname() {
-		return firstname;
-	}
-
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-
-	public String getLastname() {
-		return lastname;
-	}
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-
 }
