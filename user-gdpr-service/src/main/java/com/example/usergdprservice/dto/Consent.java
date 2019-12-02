6dda61e688c1752f9e6816237a689bcd345cba37
Thomas <thomas@t-keller.fr>
2019-12-02 21:04:02 +0100

    Implement Lombok for user-gdpr-service

--- user-gdpr-service/src/main/java/com/example/usergdprservice/dto/Consent.java
+++ user-gdpr-service/src/main/java/com/example/usergdprservice/dto/Consent.java
@@ -1,5 +1,8 @@
 package com.example.usergdprservice.dto;
 
+import lombok.Data;
+
+@Data
 public class Consent {
 	private int id;
 	private String personId;
@@ -8,45 +11,4 @@ public class Consent {
 	// Coming from GET /users
 	private String firstname;
 	private String lastname;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getPersonId() {
-		return personId;
-	}
-
-	public void setPersonId(String personId) {
-		this.personId = personId;
-	}
-
-	public boolean isOptin() {
-		return optin;
-	}
-
-	public void setOptin(boolean optin) {
-		this.optin = optin;
-	}
-
-	public String getFirstname() {
-		return firstname;
-	}
-
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-
-	public String getLastname() {
-		return lastname;
-	}
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-
 }
