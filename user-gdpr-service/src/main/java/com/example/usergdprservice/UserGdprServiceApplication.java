ec3d21751ba9ef6a748a0cc521db27f622272c9d
Thomas <thomas@t-keller.fr>
2020-04-12 19:04:04 +0200

    user-gdpr-service: enable Eureka client

--- user-gdpr-service/src/main/java/com/example/usergdprservice/UserGdprServiceApplication.java
+++ user-gdpr-service/src/main/java/com/example/usergdprservice/UserGdprServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.usergdprservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 import com.example.usergdprservice.filters.SimpleFilter;
 
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserGdprServiceApplication {
 
