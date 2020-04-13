a1eb01118b304977a1ed1830a5778bb80381feba
Thomas <thomas@t-keller.fr>
2020-04-13 13:42:06 +0200

    Enable Eureka client for all the services

--- user-gdpr-cqrs/src/main/java/com/example/usergdprcqrs/UserGdprCqrsApplication.java
+++ user-gdpr-cqrs/src/main/java/com/example/usergdprcqrs/UserGdprCqrsApplication.java
@@ -4,9 +4,11 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserGdprCqrsApplication {
 
 	@Value("${app.topics.user}")
